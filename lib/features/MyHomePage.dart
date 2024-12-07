7739a0fcad4448d84ee0f8b4fec604a2954cad62
gaurav <gaurav.bidwai99@gmail.com>
2024-12-07 12:56:25 +0530

    Improvement

--- lib/features/MyHomePage.dart
+++ lib/features/MyHomePage.dart
@@ -24,6 +24,10 @@ class _MyHomePageState extends State<MyHomePage> {
   String _scormDirectory = "";
   String _serverUrl = "http://127.0.0.1:8080";
   late String pageUrl;
+  double _downloadProgress = 0.0;
+  bool _isDownloading = false;
+  bool _isExtracting = false;
+  String _serverMessage = "";
 
   @override
   void initState() {
@@ -56,100 +60,183 @@ class _MyHomePageState extends State<MyHomePage> {
       );
   }
 
+  // Download SCORM ZIP and extract it
   Future<void> _downloadAndExtractSCORM(String url) async {
-    final Directory appDocDir = await getTemporaryDirectory();
-    _scormDirectory = '${appDocDir.path}/scorm_package/';
-    final scormDir = Directory(_scormDirectory);
-    if (!scormDir.existsSync()) {
-      scormDir.createSync(recursive: true);
-    }
+    try {
+      setState(() {
+        _isDownloading = true;
+        _downloadProgress = 0.0;
+      });
 
-    final File scormZipFile = File('${appDocDir.path}/scorm.zip');
-
-    // Download the SCORM ZIP file from the URL
-    final response = await http.get(Uri.parse(url));
-    if (response.statusCode == 200) {
-      await scormZipFile.writeAsBytes(response.bodyBytes);
-    } else {
-      throw Exception('Failed to download SCORM ZIP');
-    }
-
-    // Unzip the SCORM package
-    final bytes = await scormZipFile.readAsBytes();
-    final archive = ZipDecoder().decodeBytes(bytes);
-
-    // Write the extracted files to the directory
-    for (var file in archive) {
-      final String filename = '$_scormDirectory${file.name}';
-      final outFile = File(filename);
-      if (file.isFile) {
-        final fileDir = Directory(outFile.parent.path);
-        if (!fileDir.existsSync()) {
-          fileDir.createSync(recursive: true);
-        }
-        outFile.writeAsBytesSync(file.content as List<int>);
+      final Directory appDocDir = await getTemporaryDirectory();
+      _scormDirectory = '${appDocDir.path}/scorm_package/';
+      final scormDir = Directory(_scormDirectory);
+      if (!scormDir.existsSync()) {
+        scormDir.createSync(recursive: true);
       }
+
+      final File scormZipFile = File('${appDocDir.path}/scorm.zip');
+
+      // Download the SCORM ZIP file from the URL
+      final request = http.Client();
+      final response = await request.send(http.Request('GET', Uri.parse(url)));
+
+      final contentLength = response.contentLength;
+      int bytesReceived = 0;
+      List<int> downloadedBytes = [];
+
+      // Listen to the stream for progress
+      await for (var chunk in response.stream) {
+        bytesReceived += chunk.length;
+        downloadedBytes.addAll(chunk); // Collect the chunk data
+        setState(() {
+          _downloadProgress = contentLength != null ? bytesReceived / contentLength : 0.0;
+        });
+      }
+
+      if (response.statusCode == 200) {
+        // Save the downloaded bytes to a file
+        await scormZipFile.writeAsBytes(downloadedBytes);
+        await _extractSCORMPackage(scormZipFile);  // Pass the file to extract
+        setState(() {
+          _isDownloading = false; // Hide the downloading loader once the download is complete
+        });
+      } else {
+        throw Exception('Failed to download SCORM ZIP');
+      }
+    } catch (e) {
+      setState(() {
+        _isDownloading = false;
+        _serverMessage = "Error occurred: $e";
+      });
+      _stopServer(); // Stop the server in case of error
+      _clearFiles(); // Delete any temporary files
     }
+  }
 
-    // Now, parse the imsmanifest.xml to get the launch URL
-    final manifestFile = File('$_scormDirectory/imsmanifest.xml');
-    final manifestContent = await manifestFile.readAsString();
-    final document = xml.XmlDocument.parse(manifestContent);
+  // Extract the SCORM ZIP package
+  Future<void> _extractSCORMPackage(File scormZipFile) async {
+    try {
+      setState(() {
+        _isExtracting = true;
+      });
 
-    // Extract the href attribute from the <resource> tag
-    final href = document.findAllElements('resource').first.getAttribute('href');
-    if (href != null) {
-      // Use the href to load the starting page in the WebView
-      // _webViewController.loadRequest(Uri.parse('$_serverUrl/$href'));
-      pageUrl='$_serverUrl/$href';
-    } else {
-      throw Exception('No launch URL found in imsmanifest.xml');
+      // Extract the SCORM zip package using the saved file
+      final bytes = await scormZipFile.readAsBytes();
+      final archive = ZipDecoder().decodeBytes(bytes);
+
+      // Write the extracted files to the directory
+      for (var file in archive) {
+        final String filename = '$_scormDirectory${file.name}';
+        final outFile = File(filename);
+        if (file.isFile) {
+          final fileDir = Directory(outFile.parent.path);
+          if (!fileDir.existsSync()) {
+            fileDir.createSync(recursive: true);
+          }
+          outFile.writeAsBytesSync(file.content as List<int>);
+        }
+      }
+
+      // Now, parse the imsmanifest.xml to get the launch URL
+      final manifestFile = File('$_scormDirectory/imsmanifest.xml');
+      final manifestContent = await manifestFile.readAsString();
+      final document = xml.XmlDocument.parse(manifestContent);
+
+      // Extract the href attribute from the <resource> tag
+      final href = document.findAllElements('resource').first.getAttribute('href');
+      if (href != null) {
+        pageUrl = '$_serverUrl/$href';
+        setState(() {
+          _isExtracting = false;
+        });
+        _startServer(); // Start the server after extraction
+      } else {
+        throw Exception('No launch URL found in imsmanifest.xml');
+      }
+    } catch (e) {
+      setState(() {
+        _isExtracting = false;
+        _serverMessage = "Error occurred during extraction: $e";
+      });
+      _stopServer(); // Stop the server in case of error
+      _clearFiles(); // Delete any temporary files
     }
   }
 
   // Start a local server to serve unzipped SCORM files
   Future<void> _startServer() async {
-    final staticHandler = createStaticHandler(_scormDirectory, defaultDocument: 'index.html');
+    try {
+      setState(() {
+        _serverMessage = "Starting the server...";
+      });
 
-    final handler = const Pipeline()
-        .addMiddleware(logRequests()) // Log requests for debugging
-        .addHandler(staticHandler); // Serve static files
+      final staticHandler =
+      createStaticHandler(_scormDirectory, defaultDocument: 'index.html');
 
-    _server = await shelf_io.serve(handler, '127.0.0.1', 8080);
-    print('Server running at http://${_server.address.host}:${_server.port}');
+      final handler = const Pipeline()
+          .addMiddleware(logRequests()) // Log requests for debugging
+          .addHandler(staticHandler); // Serve static files
 
-    setState(() {
-      _serverUrl = 'http://${_server.address.host}:${_server.port}';
-      _isServerRunning = true;
-    });
+      _server = await shelf_io.serve(handler, '127.0.0.1', 8080);
+      setState(() {
+        _serverUrl = 'http://${_server.address.host}:${_server.port}';
+        _isServerRunning = true;
+        _serverMessage = 'Server running at $_serverUrl';
+      });
 
-    // Load the SCORM content into WebView
-    _webViewController..loadRequest(Uri.parse('$pageUrl'));
+      // Load the SCORM content into WebView
+      _webViewController.loadRequest(Uri.parse(pageUrl));
+    } catch (e) {
+      setState(() {
+        _serverMessage = "Error occurred while starting the server: $e";
+      });
+      _stopServer(); // Stop the server in case of error
+      _clearFiles(); // Delete any temporary files
+    }
   }
 
-  // Stop the local server
+  // Stop the server and delete SCORM directory
   void _stopServer() async {
-    // Close the server
-    await _server.close();
+    try {
+      setState(() {
+        _serverMessage = "Stopping server...";
+      });
 
-    // Delete the temporary SCORM directory and its contents
+      // Close the server
+      await _server.close();
+
+      // Delete the temporary SCORM directory and its contents
+      _clearFiles();
+
+      // Show a SnackBar to inform the user
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("SCORM files have been deleted and the server stopped.")),
+      );
+
+      // Reset WebView to a blank page or placeholder
+      _webViewController.loadHtmlString('<H1>Stopped</H1>');
+
+      // Update the state to reflect that the server is no longer running
+      setState(() {
+        _isServerRunning = false;
+        _serverMessage = "";
+      });
+    } catch (e) {
+      setState(() {
+        _serverMessage = "Error occurred while stopping the server: $e";
+      });
+    }
+  }
+
+  // Clear the downloaded SCORM files
+  void _clearFiles() {
     final scormDir = Directory(_scormDirectory);
     if (scormDir.existsSync()) {
       // Delete the directory and all its contents
       scormDir.deleteSync(recursive: true);
     }
-
-    // Show a SnackBar to inform the user
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text("SCORM files have been deleted and the server stopped.")),
-    );
-
-    // Update the state to reflect that the server is no longer running
-    setState(() {
-      _isServerRunning = false;
-    });
   }
-
 
   // Button to trigger server actions (start/stop)
   void _onStartStopButtonPressed() async {
@@ -159,7 +246,6 @@ class _MyHomePageState extends State<MyHomePage> {
       final scormUrl = _urlController.text;
       if (scormUrl.isNotEmpty) {
         await _downloadAndExtractSCORM(scormUrl);
-        _startServer();
       } else {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(content: Text("Please enter a valid SCORM URL")),
@@ -201,6 +287,24 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: _onStartStopButtonPressed,
             child: Text(_isServerRunning ? 'Stop Server' : 'Start Server'),
           ),
+          if (_isDownloading)
+            Column(
+              children: [
+                CircularProgressIndicator(),
+                Text(
+                    'Downloading: ${(_downloadProgress * 100).toStringAsFixed(2)}%'),
+              ],
+            ),
+          if (_isExtracting)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text('Extracting SCORM files...'),
+            ),
+          if (_serverMessage.isNotEmpty)
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(_serverMessage),
+            ),
           Expanded(
             child: WebViewWidget(controller: _webViewController),
           ),
